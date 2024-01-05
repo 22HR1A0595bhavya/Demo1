1770d256565d49c578df87ab0a0b0863d0c01804
22HR1A0595bhavya <154884872+22HR1A0595bhavya@users.noreply.github.com>
2024-01-05 12:03:39 +0530

    Add files via upload

---
+++ demo01.java
@@ -0,0 +1,33 @@
+class C1
+{
+public void A(){
+System.out.println("My self Bhavya");
+}
+public void B(){
+System.out.println("My stream is CSE");
+}
+public void C(){
+System.out.println("I am pusuing Btech");
+}
+public void D(){
+System.out.println("favourite language is java");
+}
+public void E(){
+System.out.println("my hobbie is reading books");
+}
+public void F(){
+System.out.println("My favourite writer is O.HENRY");
+}
+}
+class C2{
+public static void main(String[] args)
+{
+C1 ob=new C1();
+ob.A();
+ob.B();
+ob.C();
+ob.D();
+ob.E();
+ob.F();
+}
+}
---
+++ demo03.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+class Demo{
+public static void main(String args[]){
+Scanner sc=new Scanner(System.in);
+int n1=sc.nextInt();
+for(int i=0;i<10;i++)
+System.out.println(n1+"*"+i+"="+n1*i);
+}
+}
